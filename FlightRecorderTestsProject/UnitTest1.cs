075d384099439b391072dd55250f6654848e64e4
skall34 <pascal.buirey@gmail.com>
2024-02-11 11:54:07 +0100

    add empty test project

---
+++ FlightRecorderTestsProject/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace FlightRecorderTestsProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
