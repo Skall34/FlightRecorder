30557fb5639d33e330bf803710092d89ca905a8c
VHR0707 <70477313+VHR0707@users.noreply.github.com>
2024-02-27 21:26:52 +0100

    Renommage classes
    Je renomme les classes pour plus de précision

---
+++ Aeroport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightRecorder
+{
+    public class Aeroport
+    {
+        public string? Ident { get; set; }
+        public string? Type { get; set; }
+        public string? Name { get; set; }
+        public string? Municipality { get; set; }
+        public double LatitudeDeg { get; set; }
+        public double LongitudeDeg { get; set; }
+        public int ElevationFt { get; set; }
+        public string? Piste { get; set; }
+        public string? LongueurDePiste { get; set; }
+        public string? TypeDePiste { get; set; }
+        public string? Observations { get; set; }
+        public string? WikipediaLink { get; set; }
+        public int Fret { get; set; }
+        public int Pax { get; set; }
+    }
+}
---
+++ Avion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightRecorder
+{
+    public class Avion
+    {
+        public int Index { get; set; }
+        public string? ICAO { get; set; }
+        public string? Type { get; set; }
+        public string? Immat { get; set; }
+        public string? Localisation { get; set; }
+        public string? Hub { get; set; }
+        public int CoutHoraire { get; set; }
+        public int Etat { get; set; }
+        public int Status { get; set; }
+        public string? Horametre { get; set; }
+        public string? DernierUtilisateur { get; set; }
+    }
+}
