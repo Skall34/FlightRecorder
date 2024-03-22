4a032d8215a40d2c739525545bad895826158bab
skall34 <pascal.buirey@gmail.com>
2024-03-22 18:30:03 +0100

    trim end of version

--- updateVersion.ps1
+++ updateVersion.ps1
@@ -1,10 +1,13 @@
 function Update-vdproj-version([string] $ver, [string] $filename) {
+	$vItems = $ver.split(".")
+	$version = $vItems[0]+"."+$vItems[1]+"."+$vItems[2]
+	echo "using version $version"
     $productCodePattern     = '\"ProductCode\" = \"8\:{([\d\w-]+)}\"'
     $packageCodePattern     = '\"PackageCode\" = \"8\:{([\d\w-]+)}\"'
     $productVersionPattern  = '\"ProductVersion\" = \"8\:[0-9]+(\.([0-9]+)){1,3}\"'
     $productCode            = '"ProductCode" = "8:{' + [guid]::NewGuid().ToString().ToUpper() + '}"'
     $packageCode            = '"PackageCode" = "8:{' + [guid]::NewGuid().ToString().ToUpper() + '}"'
-    $productVersion         = '"ProductVersion" = "8:' + $ver + '"'
+    $productVersion         = '"ProductVersion" = "8:' + $version + '"'
 
     (Get-Content $filename) | ForEach-Object {
         % {$_ -replace $productCodePattern, $productCode } |
@@ -12,8 +15,6 @@ function Update-vdproj-version([string] $ver, [string] $filename) {
         % {$_ -replace $productVersionPattern, $productVersion }
     } | Set-Content $filename
 }
-echo $args[0]
-echo $args[1]
-
+echo "Tag version $args"
 Update-vdproj-version $args[0] "Installer.vdproj"
 
